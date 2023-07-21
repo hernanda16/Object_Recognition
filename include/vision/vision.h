ebf7ba374eade33328508b7b0f1660306c53ae06
hernanda16 <hernandaislearning@gmail.com>
2023-07-21 23:38:14 +0700

    hog, cascade, deskew

--- include/vision/vision.h
+++ include/vision/vision.h
@@ -8,8 +8,43 @@
 #include "string.h"
 #include "utils/logger.h"
 
+#define cam_path "/dev/v4l/by-id/usb-046d_C270_HD_WEBCAM_8E1A4E60-video-index0"
+
 #define haarcascade "../resources/haarcascades/haarcascade_frontalcatface_extended.xml"
 #define lbpcascade "../resources/lbpcascades/lbpcascade_frontalface_improved.xml"
+
+class _data {
+public:
+    int fps_cam, fps_real;
+
+    cv::Mat image;
+    cv::Mat crop = cv::Mat::zeros(20, 20, CV_8UC1);
+
+    std::string text_img;
+
+    boost::mutex mutex_img;
+} data;
+
+struct _cam {
+    cv::VideoCapture video;
+
+    void Init()
+    {
+        video.open(cam_path);
+        if (!video.isOpened()) {
+            std::cerr << "Error: Could not open the camera." << std::endl;
+        } else {
+            data.fps_cam = video.get(cv::CAP_PROP_FPS);
+        }
+    }
+
+    void Convert()
+    {
+        data.mutex_img.lock();
+        video >> data.image;
+        data.mutex_img.unlock();
+    }
+} cam;
 
 struct _recognition {
     /**
@@ -87,11 +122,32 @@ struct _timer {
 } timer;
 
 struct _func {
-    cv::Mat Deskew(cv::Mat& frame)
+    cv::Mat Deskew(cv::Mat frame)
     {
-        cv::Moments moment = cv::Moments(frame);
+        cv::Mat deskew_frame;
+        cv::cvtColor(frame, deskew_frame, cv::COLOR_BGR2GRAY);
+        cv::Moments moment = cv::moments(deskew_frame);
         if (abs(moment.mu02) < 1e-2) {
-            return frame.clone();
+            return deskew_frame.clone();
         }
+        float skew = moment.mu11 / moment.mu02; //central moments spreaded along xy-axes/central moments spreaded along y-axis
+        cv::Mat warp_deskew_matrix = (cv::Mat_<float>(2, 3) << 1, skew, -0.5 * 1 * skew, 0, 1, 0);
+        cv::Mat warpMat = (cv::Mat_<float>(2, 3) << 1, 0, 50, 0, 1, 50);
+        cv::Mat imgOut = cv::Mat::zeros(deskew_frame.rows, deskew_frame.cols, deskew_frame.type());
+        cv::warpAffine(deskew_frame, imgOut, warp_deskew_matrix, imgOut.size(), 1);
+
+        // cv::Mat result_nearest, result_linear, result_cubic;
+
+        // cv::warpAffine(deskew_frame, result_nearest, warpMat, deskew_frame.size(), cv::INTER_NEAREST);
+        // cv::warpAffine(deskew_frame, result_linear, warpMat, deskew_frame.size(), cv::INTER_LINEAR);
+        // cv::warpAffine(deskew_frame, result_cubic, warpMat, deskew_frame.size(), cv::INTER_CUBIC);
+
+        // cv::imshow("Nearest-Neighbor Interpolation", result_nearest);
+        // cv::imshow("Bilinear Interpolation", result_linear);
+        // cv::imshow("Bicubic Interpolation", result_cubic);
+
+        cv::imshow("imgOut", imgOut);
+
+        return imgOut;
     }
 } func;
--- src/vision.cpp
+++ src/vision.cpp
@@ -1,44 +1,44 @@
 #include "vision/vision.h"
 
-boost::mutex mutex_img;
-int fps_cam, fps_real;
-std::string text_img;
-
-int main()
+int main(int argc, char** argv)
 {
-    // cv::Mat image = cv::imread("../test_image.png");
-    cv::VideoCapture video("/dev/video2");
-    cv::Mat image;
+    // for (size_t i = 0; i < argc; i++) {
+    //     std::cout << argv[i] << std::endl;
+    // }
 
-    if (!video.isOpened()) {
-        std::cerr << "Error: Could not open the camera." << std::endl;
-    } else {
-        fps_cam = video.get(cv::CAP_PROP_FPS);
-    }
+    // data.image = cv::imread("../digits.png");
+    cam.Init();
 
     while (true) {
-
-        mutex_img.lock();
-        video >> image;
-        mutex_img.unlock();
+        cam.Convert();
 
         timer.Start();
 
-        if (!image.empty()) {
-            recognition.CascadeMethod(image, lbpcascade);
-            recognition.HogMethod(image);
+        if (!data.image.empty()) {
+            if (argc > 1) {
+                if (std::strcmp(argv[1], "0") == 0)
+                    recognition.CascadeMethod(data.image, lbpcascade);
+                else if (std::strcmp(argv[1], "1") == 0)
+                    recognition.HogMethod(data.image);
+            }
+            // Logger(CYAN, "%d %d", data.image.rows, data.image.cols);
+
+            // cv::Mat ROI(data.image, cv::Rect(0, 0, 20, 20));
+            // ROI.copyTo(data.crop);
+
+            func.Deskew(data.image);
 
             timer.Stop();
 
             if (timer.GetDuration() != 0) {
-                fps_real = 1000 / timer.GetDuration();
-                text_img = "FPS : " + std::to_string(fps_real);
+                data.fps_real = 1000 / timer.GetDuration();
+                data.text_img = "FPS : " + std::to_string(data.fps_real);
             } else {
-                text_img = "FPS : " + std::to_string(fps_cam);
+                data.text_img = "FPS : " + std::to_string(data.fps_cam);
             }
 
-            cv::putText(image, text_img, cv::Point(20, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(255), 2);
-            cv::imshow("image", image);
+            cv::putText(data.image, data.text_img, cv::Point(20, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(255), 2);
+            cv::imshow("image", data.image);
 
             char key = static_cast<char>(cv::waitKey(1));
             if (key == 'q') {
